59d9713509e699a2979a79dc2bbcb125f67d7705
Karina Hyland <khyland111@gmail.com>
2023-04-15 22:22:38 -0400

    upload script

---
+++ script.js
@@ -0,0 +1,290 @@
+let miData = [
+  {
+    "Lugar": "Kruger National Park",
+    "Latitude": -23.9883848,
+    "Longitude": 31.5547402,
+    "País": "South Africa",
+    "Población": 4000
+  },
+  {
+    "Lugar": "Serengeti National Park",
+    "Latitude": -1.8936005,
+    "Longitude": 34.6856509,
+    "País": "Tanzania",
+    "Población": 3000
+  },
+  {
+    "Lugar": "Okavango Delta",
+    "Latitude": -19.6510095,
+    "Longitude": 22.9058802,
+    "País": "Botswana",
+    "Población": 1000
+  },
+  {
+    "Lugar": "Luangwa Valley",
+    "Latitude": -12.74385799,
+    "Longitude": 31.73132229,
+    "País": "Zambia",
+    "Población": 1500
+  },
+  {
+    "Lugar": "Masai Mara National Reserve",
+    "Latitude": -1.5294931,
+    "Longitude": 35.1708741,
+    "País": "Kenya",
+    "Población": 800
+  },
+  {
+    "Lugar": "Murchison Falls National Park",
+    "Latitude": 2.2257414,
+    "Longitude": 31.7698482,
+    "País": "Uganda",
+    "Población": 1000
+  },
+  {
+    "Lugar": "Katavi National Park",
+    "Latitude": -6.8980932,
+    "Longitude": 31.1658998,
+    "País": "Tanzania",
+    "Población": 1500
+  },
+  {
+    "Lugar": "Selous Game Reserve",
+    "Latitude": -9,
+    "Longitude": 37.5,
+    "País": "Tanzania",
+    "Población": 12500
+  },
+  {
+    "Lugar": "Lower Zambezi National Park",
+    "Latitude": -15.3122767,
+    "Longitude": 29.6035495,
+    "País": "Zambia",
+    "Población": 1750
+  },
+  {
+    "Lugar": "Hwange National Park",
+    "Latitude": -19.1240539,
+    "Longitude": 26.5925913,
+    "País": "Zimbabwe",
+    "Población": 750
+  },
+  {
+    "Lugar": "Queen Elizabeth National Park",
+    "Latitude": -0.1640716,
+    "Longitude": 30.0202535,
+    "País": "Uganda",
+    "Población": 1000
+  },
+  {
+    "Lugar": "Chobe National Park",
+    "Latitude": -18.7305642,
+    "Longitude": 24.4292148,
+    "País": "Botswana",
+    "Población": 5500
+  },
+  {
+    "Lugar": "Kafue National Park",
+    "Latitude": -14.5,
+    "Longitude": 26.1666667,
+    "País": "Zambia",
+    "Población": 1750
+  },
+  {
+    "Lugar": "Liwonde National Park",
+    "Latitude": -14.844135,
+    "Longitude": 35.3466253,
+    "País": "Malawi",
+    "Población": 900
+  },
+  {
+    "Lugar": "Hippo Pool, Mara River",
+    "Latitude": -1.542518363,
+    "Longitude": 34.0306694,
+    "País": "Kenya",
+    "Población": 250
+  },
+  {
+    "Lugar": "Lake Edward",
+    "Latitude": -0.3256071,
+    "Longitude": 29.6962677,
+    "País": "Uganda",
+    "Población": 2000
+  },
+  {
+    "Lugar": "Lake Albert",
+    "Latitude": 1.7717865,
+    "Longitude": 30.9876321,
+    "País": "Uganda",
+    "Población": 1500
+  },
+  {
+    "Lugar": "Lake Naivasha",
+    "Latitude": -0.7753844,
+    "Longitude": 36.3714756,
+    "País": "Kenya",
+    "Población": 450
+  },
+  {
+    "Lugar": "Lake Manyara",
+    "Latitude": -3.6190593,
+    "Longitude": 35.8061779,
+    "País": "Tanzania",
+    "Población": 450
+  },
+  {
+    "Lugar": "Lake Kariba",
+    "Latitude": -16.9556508,
+    "Longitude": 27.9717606,
+    "País": "Zambia/Zimbabwe",
+    "Población": 6500
+  },
+  {
+    "Lugar": "Lake Victoria",
+    "Latitude": -0.7557754,
+    "Longitude": 33.438353,
+    "País": "Uganda/Tanzania/Kenya",
+    "Población": 5000
+  },
+  {
+    "Lugar": "Zambezi River",
+    "Latitude": -14.98188331,
+    "Longitude": 29.52373671,
+    "País": "Zambia/Zimbabwe/Mozambique",
+    "Población": 10000
+  },
+  {
+    "Lugar": "Senegal River Delta",
+    "Latitude": 15.2478242,
+    "Longitude": -13.66742148,
+    "País": "Senegal",
+    "Población": 1250
+  },
+  {
+    "Lugar": "Niger River Delta",
+    "Latitude": 10.68982493,
+    "Longitude": -2.027604972,
+    "País": "Mali/Niger",
+    "Población": 1750
+  },
+  {
+    "Lugar": "Bénoué National Park",
+    "Latitude": 8.368522,
+    "Longitude": 13.8680047,
+    "País": "Cameroon",
+    "Población": 750
+  },
+  {
+    "Lugar": "Hacienda Nápoles",
+    "Latitude": 5.9113419,
+    "Longitude": -74.7251751,
+    "País": "Colombia",
+    "Población": 100
+  },
+  {
+    "Lugar": "Okavango Delta",
+    "Latitude": -19.6510095,
+    "Longitude": 22.9058802,
+    "País": "Botswana",
+    "Población": 17500
+  },
+  {
+    "Lugar": "Lake Turkana",
+    "Latitude": 3.6267946,
+    "Longitude": 36.0022946,
+    "País": "Kenya",
+    "Población": 2500
+  },
+  {
+    "Lugar": "Limpopo River",
+    "Latitude": -23.6615646,
+    "Longitude": 30.27548827,
+    "País": "Botswana/Zimbabwe",
+    "Población": 4500
+  },
+  {
+    "Lugar": "Lake Tanganyika",
+    "Latitude": -6.2555679,
+    "Longitude": 29.5107708,
+    "País": "Burundi/Tanzania/Zambia",
+    "Población": 25000
+  },
+  {
+    "Lugar": "Luangwa River",
+    "Latitude": -12.74385799,
+    "Longitude": 31.73132229,
+    "País": "Zambia",
+    "Población": 12500
+  },
+  {
+    "Lugar": "Bwabwata National Park",
+    "Latitude": -18.0989125,
+    "Longitude": 21.5812969,
+    "País": "Namibia",
+    "Población": 700
+  },
+  {
+    "Lugar": "Sudd Region",
+    "Latitude": 8.3804387,
+    "Longitude": 30.7120023,
+    "País": "South Sudan",
+    "Población": 12500
+  },
+  {
+    "Lugar": "Mpala Research Centre",
+    "Latitude": 0.292366,
+    "Longitude": 36.8984604,
+    "País": "Kenya",
+    "Población": 450
+  },
+  {
+    "Lugar": "Niokolo-Koba National Park",
+    "Latitude": 13.0099988,
+    "Longitude": -12.9448205,
+    "País": "Senegal",
+    "Población": 1750
+  },
+  {
+    "Lugar": "Mana Pools National Park",
+    "Latitude": -15.8853449,
+    "Longitude": 29.4597694,
+    "País": "Zimbabwe",
+    "Población": 900
+  }
+]
+
+let miToken = "pk.eyJ1Ijoia2FyaWhpZ2giLCJhIjoiY2s5OHRxNjczMDVoejNubnhsZzFhenVpOCJ9.KSi_AKDXiNXo48NJc0Kj3A"
+
+let miMapa = L.map('mapa').setView([-33.45, -70.59], 3);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/satellite-v9',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: miToken
+  }).addTo(miMapa);
+
+for (let i = 0; i < miData.length; i++) {
+  let miLugar = miData[i];
+  let z = linearMap(miLugar.Población, 10, 25000, 25, 65);
+  //let z = miLugar.Población;
+  
+  let flagIcon = L.icon({
+    iconUrl: 'https://cdn.glitch.global/4c37d4aa-dfe7-4e52-a8c8-a5cc1c0585d6/1F99B_color.png?v=1681489219972',
+    iconSize: [z, z],
+    iconAnchor: [10, 10],
+    popupAnchor: [0, 0],
+  })
+  
+  let popupText = "<h3>" + miLugar.Lugar + ", " + miLugar.País + "</h3>" + "Población Estimada de Hipopótamos: " + miLugar.Población;
+  L.marker([miLugar.Latitude, miLugar.Longitude], {icon: flagIcon}).setOpacity(0.8).bindPopup(popupText).addTo(miMapa);
+}
+
+function linearMap(x, x1, x2, y1, y2){
+    const m = (y2 - y1) / (x2 - x1)
+    return (x - x1) * m + y1
+}
+
+
